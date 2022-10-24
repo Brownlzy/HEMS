3b0cd98b472ef0679684e4933a86e6345570fdd3
Brownlzy <73223120+Brownlzy@users.noreply.github.com>
2022-10-24 11:39:55 +0800

    HelloWorld

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.hemsteam.hems {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.hemsteam.hems to javafx.fxml;
+    exports com.hemsteam.hems;
+}
