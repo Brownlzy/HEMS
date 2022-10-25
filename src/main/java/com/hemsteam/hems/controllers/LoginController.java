192cf069d3107e0222df6213ba4baec49edf9aca
ShakingX <1542282534@qq.com>
2022-10-25 19:24:40 +0800

    增加清除功能

--- src/main/java/com/hemsteam/hems/controllers/LoginController.java
+++ src/main/java/com/hemsteam/hems/controllers/LoginController.java
@@ -3,9 +3,16 @@ package com.hemsteam.hems.controllers;
 import com.hemsteam.hems.utils.Log;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
     private static final String TAG="LoginController";
+    @FXML
+    private TextField userName;
+    @FXML
+    private PasswordField passWord;
+
     @FXML
     protected void onLoginButtonClick() {
         Log.d(TAG,"登录按钮被点击");
@@ -14,5 +21,7 @@ public class LoginController {
     @FXML
     protected void onClearButtonClick(){
         Log.d(TAG,"清除按钮被点击");
+        userName.clear();
+        passWord.clear();
     }
 }
