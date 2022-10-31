65df757dfbaf0e7b1c77641587cd325c7dda555a
Senspecial <122710612@qq.com>
2022-10-31 09:55:24 +0800

    statsController

---
+++ src/main/java/com/hemsteam/hems/controllers/StatsController.java
@@ -0,0 +1,60 @@
+package com.hemsteam.hems.controllers;
+
+import com.hemsteam.hems.HEMS;
+import com.hemsteam.hems.datamodels.Details;
+import com.hemsteam.hems.handlers.Account;
+import com.hemsteam.hems.handlers.DataBaseHelper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.*;
+
+
+import java.net.URL;
+import java.util.*;
+
+public class StatsController implements Initializable {
+    private static final String TAG = "StatsController";
+
+    @FXML
+    private BarChart barchart;
+
+
+
+
+
+
+
+
+
+
+
+    @FXML
+    void onInquireClick(ActionEvent event) {
+
+
+    }
+
+
+    private CategoryAxis xAxis;
+
+    private ObservableList<String> typeNames = FXCollections.observableArrayList();
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
