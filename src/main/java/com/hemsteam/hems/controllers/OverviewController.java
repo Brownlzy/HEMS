ce63c5c710b0825d63b84362d08d06a808c996e7
ShakingX <1542282534@qq.com>
2022-11-01 18:49:46 +0800

    修复新用户总览页面饼状图无法画的Bug

--- src/main/java/com/hemsteam/hems/controllers/OverviewController.java
+++ src/main/java/com/hemsteam/hems/controllers/OverviewController.java
@@ -38,41 +38,48 @@ public class OverviewController implements Initializable {
             total.setText(String.valueOf(DataBaseHelper.getInstance().getTypeMoneyMap(Account.getUser(), Account.getYear(), Account.getMonth()).get("AllType")));
             Log.d(this.getClass(), "数据读出");
             HashMap<String,Double>optimizeData=getOptimizeData();
-        for (String key:
-                optimizeData.keySet()) {
-            sum=sum+optimizeData.get(key);
-        }
+        if (optimizeData==null){
+            pieChart.setTitle("无支出");
+        }else {
+            for (String key :
+                    optimizeData.keySet()) {
+                sum = sum + optimizeData.get(key);
+            }
             ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-        for (String key:
-                optimizeData.keySet()) {
-            tag=key+Percent.getPercent(
-                    optimizeData.get(key),
-                    sum);
-            pieChartData.add(new PieChart.Data(tag,optimizeData.get(key)));
-        }
+            for (String key :
+                    optimizeData.keySet()) {
+                tag = key + Percent.getPercent(
+                        optimizeData.get(key),
+                        sum);
+                pieChartData.add(new PieChart.Data(tag, optimizeData.get(key)));
+            }
             pieChart.setData(pieChartData);
+        }
     }
     /**
      * 筛选出大于5%的消费类型值
      * @return HashMap
      */
-    public HashMap<String ,Double> getOptimizeData(){
-        HashMap<String,Double>originData;
-        HashMap<String,Double>optimizeData=new HashMap<>();
-        originData=DataBaseHelper.getInstance().getTypeMoneyMap(Account.getUser(), Account.getYear(), Account.getMonth());
-        double other=originData.get("AllType");
-        for (String key:
-             originData.keySet()) {
-            if(!key.equals("AllType")&&originData.get(key)>(originData.get("AllType")*0.05)){
-                optimizeData.put(key,originData.get(key));
-                other=other-originData.get(key);
+    public HashMap<String ,Double> getOptimizeData() {
+        HashMap<String, Double> originData;
+        HashMap<String, Double> optimizeData = new HashMap<>();
+        originData = DataBaseHelper.getInstance().getTypeMoneyMap(Account.getUser(), Account.getYear(), Account.getMonth());
+        if (!originData.containsKey("AllType")) {
+            return null;
+        } else {
+            double other = originData.get("AllType");
+            for (String key :
+                    originData.keySet()) {
+                if (!key.equals("AllType") && originData.get(key) > (originData.get("AllType") * 0.05)) {
+                    optimizeData.put(key, originData.get(key));
+                    other = other - originData.get(key);
+                }
             }
+            if (optimizeData.containsKey("其它")) {
+                optimizeData.put("其它", optimizeData.get("其它") + other);
+            } else
+                optimizeData.put("其它", other);
+            return optimizeData;
         }
-        if(optimizeData.containsKey("其它")) {
-            optimizeData.put("其它", optimizeData.get("其它") + other);
-        }
-        else
-            optimizeData.put("其它",other);
-        return optimizeData;
     }
 }
--- src/main/java/com/hemsteam/hems/controllers/StatsController.java
+++ src/main/java/com/hemsteam/hems/controllers/StatsController.java
@@ -104,7 +104,6 @@ public class StatsController implements Initializable {
 
 
 
-
     }
 }
 
